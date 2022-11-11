f5da5261bf3b59ee339215651aa8a20c25b4446e
AyahSidu <46842252+AyahSidu@users.noreply.github.com>
2022-11-11 20:12:41 +0200

    Add files via upload

---
+++ data types.py
@@ -0,0 +1,28 @@
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValuee=3.14
+print(myValuee)
+print(type(myValuee))
+print(str(myValuee) + " is of the data type " + str(type(myValuee)))
+
+myValu=5j
+print(myValu)
+print(type(myValu))
+print(str(myValu) + " is of the data type " + str(type(myValu)))
+
+myVal=True
+print(myVal)
+print(type(myVal))
+print(str(myVal) + " is of the data type " + str(type(myVal)))
+
+
+myVa=False
+print(myVa)
+print(type(myVa))
+print(str(myVa) + " is of the data type " + str(type(myVa)))
+
+
+
---
+++ numeric-data.py
@@ -0,0 +1,11 @@
+x = 2 + 2
+print(x)
+
+y=(4-2)
+print(y)
+
+z=(2*2)
+print(z)
+
+m=(4/2)
+print(m)
